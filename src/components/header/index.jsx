d881ac5c2d05bd4fdb6fb2e4482dced298d6d912
HollowZll <118689735+HollowZll@users.noreply.github.com>
2023-11-13 18:41:49 -0600

    add: navbar beginning

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,24 @@
-const HeaderComponent = () => {
-    return (
-        <>
-            <h1>Header</h1>
-        </>
-    )
-}
+import './header.css'
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default HeaderComponent;
+function HeaderComponent() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home" className='logo'></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+  
+  export default HeaderComponent;
