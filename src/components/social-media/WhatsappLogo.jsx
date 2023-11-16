c80a1ad0fb9e439ab0bba103d65f4630a19c67eb
MayRdz <altamiranormay@gmail.com>
2023-11-16 13:51:25 -0700

    aling social media icons

--- src/components/social-media/WhatsappLogo.jsx
+++ src/components/social-media/WhatsappLogo.jsx
@@ -3,11 +3,11 @@ function WhatsappLogo () {
 
     return (
         <div>
-            <svg fill="#E91E63" width="24px" height="24px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <svg fill="#E91E63" width="26px" height="26px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 
-            <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+            <g id="SVGRepo_bgCarrier"/>
 
-            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+            <g id="SVGRepo_tracerCarrier"/>
 
             <g id="SVGRepo_iconCarrier">
 
