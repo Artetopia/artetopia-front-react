b62afd5018ae8917988cd7b99dab1bc6f3933029
GuadalupeAngelesDev <guadalupeangeles.av20@gmail.com>
2023-11-16 18:26:42 -0600

    Section Cards About paddings

--- src/components/cards_about/index.jsx
+++ src/components/cards_about/index.jsx
@@ -10,11 +10,11 @@ const CardsAbout = ({title,text,background_img}) => {
               }}
               className={`text-center ${background_img} position-relative`}
             >
-              <CardBody className='position-absolute bottom-0 start-0'>
-                <CardTitle tag="h5">
+              <CardBody className='position-absolute bottom-0 start-0 '>
+                <CardTitle tag="h5" className='px-3 pb-1'>
                  {title}
                 </CardTitle>
-                <CardText className='p-3'>
+                <CardText className='px-3'>
                   {text}
                 </CardText>
               </CardBody>
