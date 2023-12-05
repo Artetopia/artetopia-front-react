7dba6a7c49378833e3bcc485198296b38b856d35
MayRdz <altamiranormay@gmail.com>
2023-12-05 16:43:27 -0700

    adding border color at progress bar mobile/tablet

--- src/components/Multistep/Multistep.jsx
+++ src/components/Multistep/Multistep.jsx
@@ -33,15 +33,16 @@ const [profilePic, setProfilePic] = useState();
     <Form onSubmit={handleSubmit}>
       {step === 1 && 
       
-      <div className='circle-step rounded-circle m-2'>
-        <svg className='d-flex justify-content-center align-items-center m-1' width="22px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#E91E63" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="#E91E63" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+      <div className='circle-step rounded-circle d-lg-none m-2'>
+        <svg className='user-icon-svg d-flex justify-content-center align-items-center m-1 m-md-2' width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#E91E63" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="#E91E63" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
       </div>
       }
 
       {step === 2 && 
       <h1>Paso2</h1>
       }
-      <ProgressBar label={step} variant="custom" now={(step / 7) * 100} />
+
+      <ProgressBar className='border-progress-bar d-lg-none' label={step} variant="custom" now={(step / 7) * 100} />
       {step === 1 && (
         <Form.Group controlId="formStep1">
           <Form.Label className='subtitle-text d-flex justify-content-center'>Informacion personal</Form.Label>
