49f39347bed71413cdc7cc24e52cd5ab41beb8dd
MayRdz <altamiranormay@gmail.com>
2023-12-10 17:54:10 -0700

    adding star rating at craftsman's cards

--- src/components/CardCraftsman/index.jsx
+++ src/components/CardCraftsman/index.jsx
@@ -4,11 +4,19 @@ const CardCraftsman = ({websiteName, profilePic, rate, categories}) => {
     return (
         <div className="content-craftsman-container pt-4 pb-1 px-2">
             <div className="card-craftsman-container rounded-4">
-                <h5 className='website-name-label text-center'>{websiteName}</h5>
-                <img className='website-profilePic rounded-circle border-2 d-block' src={profilePic} alt={`Foto de perfil de ${websiteName}`}/>
-                <p>{rate}</p>
+                <h5 className='website-name-label text-center py-3'>{websiteName}</h5>
+                <img className='website-profilePic rounded-circle my-3 border-2 d-block' src={profilePic} alt={`Foto de perfil de ${websiteName}`}/>
+                <div className='d-flex justify-content-center py-3'>
+                    {rate == 0.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 1 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
+                    {rate == 1.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 2 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
+                    {rate == 2.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 3 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
+                    {rate == 3.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 4 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
+                    {rate == 4.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 5 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
+                </div>
                 <p>{categories}</p>
+
             </div>
+            
         </div>
     )}
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ const DashboardClient = () => {
     const website = { 
         name: 'ARTE | SANO',
         profilePic: 'https://img.freepik.com/foto-gratis/florero-pintura-hombre-vista-frontal_23-2149644938.jpg?w=740&t=st=1702256764~exp=1702257364~hmac=04e5c3574d858e57c9a80d7af4d76e807685950d856b190ae64ff59f8f2ac88c',
-        rate: '4',
+        rate: '3.5',
         categories: ['Accesorios', 'Ropa', 'Bolsas', 'Mantel', 'Textil']
       }
     return (
@@ -22,7 +22,7 @@ const DashboardClient = () => {
         <div className="d-flex justify-content-center">
         <div className="searchbar-container my-3">
             <input type="text" className="form-control border-1 rounded-5" placeholder='Buscar'/>
-            {/* <i className="fa fa-search"></i>*/}
+            <i className="fa fa-search"></i>
         </div>
         </div>
         <CardCraftsman
