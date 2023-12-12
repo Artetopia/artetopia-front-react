29702d0bf1eeb1e4c8a671a09969692b4a27106f
MayRdz <altamiranormay@gmail.com>
2023-12-12 10:28:33 -0700

    adding categories' buttons and making searchbar a component

--- src/components/CardCraftsman/index.jsx
+++ src/components/CardCraftsman/index.jsx
@@ -1,21 +1,28 @@
 import "./CardCraftsman.scss"
 
 const CardCraftsman = ({websiteName, profilePic, rate, categories}) => {
-
+    // console.log(categories )
     return (
-        <div className="content-craftsman-container pt-4 pb-1 px-2">
+        <div className="content-craftsman-container d-flex justify-content-center py-3">
             <div className="card-craftsman-container rounded-4">
                 <h5 className='website-name-label text-center py-3'>{websiteName}</h5>
                 <img className='website-profilePic rounded-circle my-1 border-2 d-block' src={profilePic} alt={`Foto de perfil de ${websiteName}`}/>
-                <div className='d-flex justify-content-center py-3'>
-                    {rate == 0.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 1 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
-                    {rate == 1.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 2 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
-                    {rate == 2.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 3 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
-                    {rate == 3.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 4 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
-                    {rate == 4.5 ? <i className="text-warning fa fa-star-half-empty"/> : rate >= 5 ? <i className="text-warning fa fa-star"/> : <i className="text-warning fa fa-star-o"/>}
+                <div className='d-flex justify-content-center py-3 my-3 text-warning'>
+                    {rate == 0.5 ? <i className="mx-1 fa fa-star-half-empty"/> : rate >= 1 ? <i className="mx-1 fa fa-star"/> : <i className="mx-1 fa fa-star-o"/>}
+                    {rate == 1.5 ? <i className="mx-1 fa fa-star-half-empty"/> : rate >= 2 ? <i className="mx-1 fa fa-star"/> : <i className="mx-1 fa fa-star-o"/>}
+                    {rate == 2.5 ? <i className="mx-1 fa fa-star-half-empty"/> : rate >= 3 ? <i className="mx-1 fa fa-star"/> : <i className="mx-1 fa fa-star-o"/>}
+                    {rate == 3.5 ? <i className="mx-1 fa fa-star-half-empty"/> : rate >= 4 ? <i className="mx-1 fa fa-star"/> : <i className="mx-1 fa fa-star-o"/>}
+                    {rate == 4.5 ? <i className="mx-1 fa fa-star-half-empty"/> : rate >= 5 ? <i className="mx-1 fa fa-star"/> : <i className="mx-1 fa fa-star-o"/>}
                 </div>
-                <p>{categories}</p>
-                {/* {categories.forEach(category =>  console.log(category ) )} */}
+                <div className="container">
+                    <div className="row">
+                        {categories.map(category => (
+                            <div className='col d-flex justify-content-evenly justify-content-md-center' key={category}>
+                                <button className='categories-button-container d-fill text-center rounded-2 my-3 px-2 mx-md-3 px-md-4'>{category}</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
             
         </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import CardCraftsman from "../components/CardCraftsman";
+import Searchbar from "../components/Searchbar";
 import "./dashboardClient.scss"
 
 const DashboardClient = () => {
@@ -12,7 +13,7 @@ const DashboardClient = () => {
         name: 'ARTESANOS LEON',
         profilePic: 'https://img.freepik.com/foto-gratis/sastre-creativo-trabajando-taller_23-2148970726.jpg?w=740&t=st=1702279290~exp=1702279890~hmac=234009d0e7d4fe552e54f27a6d2efaa680f75e4b09addae1436d0d7959a078a9',
         rate: '1.5',
-        categories: ['Joyeria', 'Cuero', 'Bolsas', 'Botas', 'Textil']
+        categories: ['Cuero', 'Bolsas', 'Botas', 'Textil']
       }]
 
       
@@ -22,16 +23,13 @@ const DashboardClient = () => {
             <h3 className='principal-title d-flex justify-content-center'>Las mejores artesanías de México</h3>
             <p className='subtitle-text d-flex justify-content-center'>+1000 artesanos de todas las partes de México</p>
         </div>
-        <div className="d-flex justify-content-evenly">
-            <button className='filter-buttons rounded-3'>Todos</button>
-            <button className='filter-buttons rounded-3'>Tendencia</button>
-            <button className='filter-buttons rounded-3'>Recientes</button>
+        <div className="d-flex justify-content-evenly justify-content-md-center">
+            <button className='filter-buttons rounded-3 mx-md-4'>Todos</button>
+            <button className='filter-buttons rounded-3 mx-md-4'>Tendencia</button>
+            <button className='filter-buttons rounded-3 mx-md-4'>Recientes</button>
         </div>
         <div className="d-flex justify-content-center">
-        <div className="searchbar-container my-3">
-            <input type="text" className="form-control border-1 rounded-5" placeholder='Buscar'/>
-            <i className="fa fa-search"></i>
-        </div>
+            <Searchbar/>
         </div>
         <CardCraftsman
         websiteName={websites[0].name}
@@ -39,6 +37,7 @@ const DashboardClient = () => {
         rate={websites[0].rate}
         categories={websites[0].categories}
         />
+
         <CardCraftsman
         websiteName={websites[1].name}
         profilePic={websites[1].profilePic}
