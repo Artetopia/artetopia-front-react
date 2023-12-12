9eb9885e0063aaa2293a23337cc3d937caf3f55e
MayRdz <altamiranormay@gmail.com>
2023-12-12 11:14:09 -0700

    making md responsive

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,6 +3,7 @@ import Searchbar from "../components/Searchbar";
 import "./dashboardClient.scss"
 
 const DashboardClient = () => {
+    const categories = ['Joyería', 'Textil', 'Cerámica', 'Ropa', 'Zapatos', 'Decoración', 'Accesorios']
     const websites = [{ 
         name: 'ARTE | SANO',
         profilePic: 'https://img.freepik.com/foto-gratis/florero-pintura-hombre-vista-frontal_23-2149644938.jpg?w=740&t=st=1702256764~exp=1702257364~hmac=04e5c3574d858e57c9a80d7af4d76e807685950d856b190ae64ff59f8f2ac88c',
@@ -19,17 +20,35 @@ const DashboardClient = () => {
       
     return (
         <>
+        <div className="d-flex justify-content-center">
+            <div className="top-categories-container d-none d-lg-block">
+                <hr className="horizontal-row-container" />
+                <div className="container">
+                    <div className="row">
+                        <div className='col d-flex justify-content-between align-items-center'>
+                            {categories.map(category => (
+                                <div key={category}>
+                                    <button className='categories-button-container text-center rounded-1 my-1 px-1'>{category}</button>
+                                </div>
+                            ))}
+                            <Searchbar  />
+                        </div>
+                    </div>
+                </div>
+                <hr className="horizontal-row-container" />
+            </div>
+        </div>
         <div className="title-head text-center">
             <h3 className='principal-title d-flex justify-content-center'>Las mejores artesanías de México</h3>
             <p className='subtitle-text d-flex justify-content-center'>+1000 artesanos de todas las partes de México</p>
         </div>
-        <div className="d-flex justify-content-evenly justify-content-md-center">
+        <div className="d-flex justify-content-evenly justify-content-md-center my-lg-5">
             <button className='filter-buttons rounded-3 mx-md-4'>Todos</button>
             <button className='filter-buttons rounded-3 mx-md-4'>Tendencia</button>
             <button className='filter-buttons rounded-3 mx-md-4'>Recientes</button>
         </div>
-        <div className="d-flex justify-content-center">
-            <Searchbar/>
+        <div className="d-flex justify-content-center d-lg-none">
+            <Searchbar />
         </div>
         <CardCraftsman
         websiteName={websites[0].name}
