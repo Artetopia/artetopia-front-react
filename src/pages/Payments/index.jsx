a227d104a2c398a85d8d98e00f4bbb436bd2263d
MayRdz <altamiranormay@gmail.com>
2023-12-21 16:52:58 -0700

    changing styles button

--- src/pages/Payments/index.jsx
+++ src/pages/Payments/index.jsx
@@ -6,7 +6,7 @@ const payments = () => {
         <div className=' m-5'>
             <p className='payments-text text-center text-uppercase'>En el siguiente link podras ver todos los pagos que se te han realizado y podras modificar la información dentro del panel de pagos</p>
             <div className="d-flex d-flex justify-content-center justify-content-lg-end ">
-                <button className='show-dashboard-button rounded-2 p-3 '>Ver Dashboard</button>
+                <button className='show-dashboard-button rounded-2 py-2 px-3'>Ver Dashboard</button>
             </div>
         </div>
         </>
