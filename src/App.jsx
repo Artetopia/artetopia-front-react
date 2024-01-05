a69473085befc4d7a63b758b74535deaa4d730d6
MayRdz <altamiranormay@gmail.com>
2024-01-05 12:39:40 -0700

    align items at modalFeedback

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,22 @@ import OrdersCraftsman from "./pages/OrdersCraftsman"
 import Page3 from "./pages/Page3";
 import DeliveryStatus from "./pages/DeliveryStatus"
 import DashboardClient from "./pages/Dashboard-cliente"
+import ModalFeedback from "./components/ModalFeedback";
 
 function App() {
+  const websites = [{ 
+    name: 'ARTE | SANO',
+    profilePic: 'https://img.freepik.com/foto-gratis/florero-pintura-hombre-vista-frontal_23-2149644938.jpg?w=740&t=st=1702256764~exp=1702257364~hmac=04e5c3574d858e57c9a80d7af4d76e807685950d856b190ae64ff59f8f2ac88c',
+  },
+  { 
+    name: 'ARTESANOS LEON',
+    profilePic: 'https://img.freepik.com/foto-gratis/sastre-creativo-trabajando-taller_23-2148970726.jpg?w=740&t=st=1702279290~exp=1702279890~hmac=234009d0e7d4fe552e54f27a6d2efaa680f75e4b09addae1436d0d7959a078a9',
+  },
+  { 
+    name: 'FLOR DE CANELA',
+    profilePic: 'https://img.freepik.com/foto-gratis/bodegon-herramientas-ceramica-alfareria_23-2150197287.jpg?w=360&t=st=1702434457~exp=1702435057~hmac=a87e51552a2df4bd5ea943d5d2b2c87cdae059fe60fbe12bef97830145f45490',
+  }
+]
     const showMore = () => {
         if(numberOfItemsArtesano + 3 <= artesanos.length) {
             setNumberOfItemsArtesano(numberOfItemsArtesano + 3);
@@ -189,8 +203,11 @@ function App() {
     </header>
 
     <section id='hero-section'>
-    <DashboardClient/>
-      <HeaderComponent></HeaderComponent>
+<ModalFeedback
+websiteName = {websites[0].name}
+profilePic = {websites[0].profilePic}
+/>
+        <HeaderComponent></HeaderComponent>
     </section>
     
     <section id='about' className="mb-4">
