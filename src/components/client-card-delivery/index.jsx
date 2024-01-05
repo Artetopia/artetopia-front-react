264e1a2811ac1fc072a5ddbd213cc840a1e9c6c1
HollowZll <118689735+HollowZll@users.noreply.github.com>
2024-01-05 14:39:08 +0100

    add: final touches to components

--- src/components/client-card-delivery/index.jsx
+++ src/components/client-card-delivery/index.jsx
@@ -17,23 +17,23 @@ const ClientCard = () => {
     <>
 
       <div className='main-container'>
-      <section className='d-flex align-items-center justify-content-between'>
+      <section className='d-flex align-items-center justify-content-between m-2 p-5'>
         <h3>Mis pedidos</h3>
-        <div className="d-flex p-5">
+        <div className="d-flex justify-content-center align-items-center p-5">
           <Dropdown isOpen={dropdownOpen} toggle={toggle}>
             <DropdownToggle caret>Dropdown</DropdownToggle>
             <DropdownMenu>
               <DropdownItem header>Months</DropdownItem>
-              <DropdownItem>3 Months</DropdownItem>
-              <DropdownItem text>6 Months</DropdownItem>
-              <DropdownItem>9 Months</DropdownItem>
+              <DropdownItem text>3 Months</DropdownItem>
+              <DropdownItem text >6 Months</DropdownItem>
+              <DropdownItem text>9 Months</DropdownItem>
             </DropdownMenu>
           </Dropdown>
         </div>
       </section>
 
 
-      <section className='d-flex flex-wrap'>
+      <section className='d-flex flex-wrap justify-content-center'>
       <div className="card">
         <div className="cardImage">
           <img src={CardImage} alt="pedido" className="cardImage" />
