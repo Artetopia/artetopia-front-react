db75b6c7c51295234f32e96b8a295f21eb720359
David Krumholz <davidkrumholz@yahoo.com.mx>
2024-01-07 13:12:20 -0600

    Changes of file styles scss

--- src/pages/product-carousel/ImageGallery.jsx
+++ src/pages/product-carousel/ImageGallery.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import Imagen1 from '../../../public/imagen1.png';
-import Imagen2 from '../../../public/imagen2.jpeg';
-import Imagen3 from '../../../public/imagen3.jpeg';
-import Imagen6 from '../../../public/imagen6.png';
-import Carrito from '../../../public/carrito.svg';
-import "./styles.css"
+import React, { useState } from "react";
+import ButtonAction from "../../components/buttonAction";
+import Imagen1 from "../../../public/imagen1.png";
+import Imagen2 from "../../../public/imagen2.jpeg";
+import Imagen3 from "../../../public/imagen3.jpeg";
+import Imagen6 from "../../../public/imagen6.png";
+import "./styles.css";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const ImageGallery = () => {
-  const images = [Imagen1, Imagen2, Imagen3];  // 
+  const images = [Imagen1, Imagen2, Imagen3]; //
 
   // State to keep track of the current main image index
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -18,64 +19,74 @@ const ImageGallery = () => {
   };
 
   return (
-
     <>
-    <div className="container">
-          <section className='username'>
-        <img src={Imagen6} alt="user_image" className='user_image' />
-        <h1 className='username_tittle'>Jose Mungüia</h1>
-    </section>
-    
-    <div className='card_flex'>
-    <section className='section_product'>
-    <div style={{ textAlign: 'center' }}>
-      {/* Main Viewfinder */}
-    <div className='flex-changes'>
-       <section className='title-image-text'>
-       <h2 className='product_title'>Juego de vasos y jarra</h2>
-       <p className='paragraph'>Juego de vasos en hechos en barro pintados con diseño artesanal mexicano </p>
-      <div>
-        <img src={images[currentIndex]} alt="Main Viewfinder" className='main-image' />
+      <div className="container">
+        <section className="username">
+          <img src={Imagen6} alt="user_image" className="user_image" />
+          <h1 className="username_tittle">Jose Mungüia</h1>
+        </section>
+
+        <div className="card_flex">
+          <section className="section_product">
+            <div style={{ textAlign: "center" }}>
+              {/* Main Viewfinder */}
+              <div className="flex-changes">
+                <section className="title-image-text">
+                  <h2 className="product_title">Juego de vasos y jarra</h2>
+                  <p className="paragraph">
+                    Juego de vasos en hechos en barro pintados con diseño
+                    artesanal mexicano
+                  </p>
+                  <div>
+                    <img
+                      src={images[currentIndex]}
+                      alt="Main Viewfinder"
+                      className="main-image"
+                    />
+                  </div>
+                </section>
+
+                {/* Small Images Carousel */}
+                <div
+                  className="carousel-flex"
+                  style={{
+                    overflowX: "scroll",
+                    overflowY: "scroll",
+                    whiteSpace: "nowrap",
+                    marginTop: "10px",
+                  }}
+                >
+                  {images.map((image, index) => (
+                    <img
+                      key={index}
+                      src={image}
+                      alt={`Small Image ${index + 1}`}
+                      // style={{ width: '100px', height: '100px', cursor: 'pointer', marginRight: '5px' }}
+                      onClick={() => handleImageClick(index)}
+                      className="carrousel-image"
+                    />
+                  ))}
+                </div>
+              </div>
+
+              <section className="call-to-action">
+                <h3 className="price">$450</h3>
+
+                <div className="button-comprar">
+                  <ButtonAction
+                    text="Comprar"
+                    icon={faCartShopping}
+                    type="button"
+                    buttonClass="button-primary"
+                  ></ButtonAction>
+                </div>
+              </section>
+            </div>
+          </section>
+        </div>
       </div>
-      </section>
-
-      {/* Small Images Carousel */}
-      <div  className="carousel-flex" style={{ overflowX: 'scroll', whiteSpace: 'nowrap', marginTop: '10px'}}>
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Small Image ${index + 1}`}
-            // style={{ width: '100px', height: '100px', cursor: 'pointer', marginRight: '5px' }}
-            onClick={() => handleImageClick(index)}
-            className='carrousel-image'
-          />
-        ))}
-      </div>
-
-    </div>
-
-      <section className='call-to-action'>
-      <h3 className='price'>$450</h3>
-      
-      <div className='button-comprar'>
-      <button className='comprar'><img src={Carrito} className='carrito'></img>Comprar</button>
-      </div>
-      </section>
-      
-
-    </div>
-
-    </section>
-    </div>
-    </div>
-
     </>
   );
 };
 
 export default ImageGallery;
-
-
-
-
