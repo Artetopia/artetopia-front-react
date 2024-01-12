76c5242feb16453f8f17ff5014178f45abe17e6d
GuadalupeAngelesDev <guadalupeangeles.av20@gmail.com>
2024-01-12 17:49:00 -0600

    Changes styles

--- src/components/gallery_template_b/index.jsx
+++ src/components/gallery_template_b/index.jsx
@@ -1,51 +1,54 @@
 import "./styles/styles.css"
-const GalleryTemplateB = ({ img1, img2, img3, img4,img5, img6 }) => {
-    return (
-        <>
-           <div className="row h-75 gallery">
-              <div className="col-lg-4 col-md-12">
-                <img
-                  src={img1}
-                  className="shadow-1-strong rounded mb-4 imgx "
-                  alt="Boat on Calm Water"
-                />
+const GalleryTemplateB = ({ img1, img2, img3, img4, img5, img6 }) => {
+  return (
+    <>
+      <div className="d-none d-lg-block"> 
 
-                <img 
-                  src={img2}
-                  className="shadow-1-strong rounded mb-4 imgx"
-                  alt="Wintry Mountain Landscape"
-                />
-              </div>
+        <div className="row h-75 gallery ">
+          <div className="col-lg-4 col-md-12">
+            <img
+              src={img1}
+              className="shadow-1-strong rounded mb-4 imgx "
+              alt="Boat on Calm Water"
+            />
 
-              <div className="col-lg-4">
-                <img id="imgy"
-                  src={img3}
-                  className="shadow-1-strong rounded mb-4"
-                  alt="Mountains in the Clouds"
-                />
+            <img
+              src={img2}
+              className="shadow-1-strong rounded mb-4 imgx"
+              alt="Wintry Mountain Landscape"
+            />
+          </div>
 
-                <img id="imgy1"
-                  src={img4}
-                  className=" shadow-1-strong rounded mb-4"
-                  alt="Boat on Calm Water"
-                />
-              </div>
+          <div className="col-lg-4">
+            <img id="imgy"
+              src={img3}
+              className="shadow-1-strong rounded mb-4"
+              alt="Mountains in the Clouds"
+            />
 
-              <div className="col-lg-4">
-                <img 
-                  src={img5}
-                  className="shadow-1-strong rounded mb-4 imgx"
-                  alt="Waves at Sea"
-                />
+            <img id="imgy1"
+              src={img4}
+              className=" shadow-1-strong rounded mb-4"
+              alt="Boat on Calm Water"
+            />
+          </div>
 
-                <img 
-                  src={img6}
-                  className="shadow-1-strong rounded mb-4 imgx"
-                  alt="Yosemite National Park"
-                />
-              </div>
-            </div>
-        </>
-    )
+          <div className="col-lg-4">
+            <img
+              src={img5}
+              className="shadow-1-strong rounded mb-4 imgx"
+              alt="Waves at Sea"
+            />
+
+            <img
+              src={img6}
+              className="shadow-1-strong rounded mb-4 imgx"
+              alt="Yosemite National Park"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 export default GalleryTemplateB
